9465755c84aafd0c9743046006e73cdd05faceea
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-01-03 19:30:57 +0800

    Update HeroSection.tsx

--- apps/blog/src/app/components/sections/HeroSection.tsx
+++ apps/blog/src/app/components/sections/HeroSection.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 const HeroSection: React.FC = () => (
-  <div className="w-full h-[250px] flex flex-col justify-between items-center text-[var(--color-text-main)]">
-    <div className="px-8 pt-6 rounded-lg flex items-start w-full ml-8 ">
+  <div className="w-full h-[250px] flex flex-col justify-start items-start text-[var(--color-text-main)]">
+    <div className="rounded-lg flex items-start w-full">
       <div>
-        <p className="mt-20 mb-4 text-[30px] font-bold tracking-wide text-[var(--color-primary)]">
-          Blog
+        <p className="text-[30px] font-bold tracking-wide text-[var(--color-primary)]">
+          Joem's Random Topics
         </p>
-        <p className="text-base text-[var(--color-text-subtle)] leading-relaxed italic">
-          Test trestsetserfsaef
+        <p className="text-base text-[var(--color-text-subtle)] leading-relaxed italic flex">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
-
       </div>
     </div>
   </div>
