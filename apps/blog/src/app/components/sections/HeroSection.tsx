4a2deca3dfa1e777775762d89587065039501e4e
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-01-07 19:27:21 +0800

    Update HeroSection.tsx

--- apps/blog/src/app/components/sections/HeroSection.tsx
+++ apps/blog/src/app/components/sections/HeroSection.tsx
@@ -1,17 +1,21 @@
 "use client";
+import Image from "next/image";
 
 const HeroSection: React.FC = () => (
-  <div className="w-full h-[250px] flex flex-col justify-start items-start text-[var(--color-text-main)]">
-    <div className="rounded-lg flex items-start w-full">
-      <div>
-        <p className="text-[30px] font-bold tracking-wide text-[var(--color-primary)]">
-          Joem's Random Topics
+  <div className="w-full h-full flex flex-col justify-start items-center text-[var(--color-text-main)]">
+        <Image
+                src={"/image1.jpg"}
+                alt="hero"
+                width={200}
+                height={200}
+                className="max-w-[200px] max-h-[200px] rounded-md object-cover"
+              />
+        <p className="text-[30px] text-center font-bold tracking-wide text-[var(--color-primary)]">
+          A Collection of Notes, Ideas, and Experiments
         </p>
-        <p className="text-base text-[var(--color-text-subtle)] leading-relaxed italic flex">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-      </div>
-    </div>
+        <p className="text-base text-center text-[var(--color-text-subtle)] leading-relaxed italic flex">
+          Writing about code, learning, side projects, and whatever else seems worth understanding better.
+       </p>
   </div>
 );
 
