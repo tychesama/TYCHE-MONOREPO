fef7d36cc0264390c0c8a4ae8708b814fc8120c4
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-01-17 17:25:14 +0800

    Create CertificationModal.tsx

---
+++ apps/portfolio-website/src/components/modal/CertificationModal.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+
+interface Certification {
+  name: string;
+  logo: string;
+  image?: string;
+  issuer?: string;
+  date?: string;
+  details?: string;
+}
+
+interface CertificationsCardProps {
+  certification: Certification;
+}
+
+const CertificationModal = () => {
+  return (
+    <div className="flex flex-col gap-4 w-[900px]">
+      <div className="flex flex-row items-center justify-center h-[284px]">
+        <div className="bg-[--color-card] py-2 px-3 h-[284px] flex flex-col items-center gap-3 shadow-sm rounded-lg">
+          <img
+            src="/assets/ccna.jpg"
+            alt="logo"
+            className="w-[200px] h-[200px] rounded-lg object-cover"
+          />
+          <p>Jairosoft Inc</p>
+          <p>Bubble Developer</p>
+
+        </div>
+        <div className="py-2 px-3 h-[284px] flex items-center gap-3 shadow-sm hover:shadow-md transition-shadow duration-150">
+          <img
+            src="/assets/sss.jpg"
+            alt="Loading..."
+            className="w-[200px] h-[200px] rounded-lg object-cover"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-start justify-start h-[215px]">
+        <div>
+          <p>About the Company</p>
+          <p> descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long description</p>
+        </div>
+        <div>
+          <p>About the Company</p>
+          <p> Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long description</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CertificationModal;
