4b5c6bee7e63f230ca38d36af1eac2a0fc0a0a20
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-02-17 15:46:14 +0800

    fix: portfolio - font finally

--- apps/portfolio-website/src/app/layout.tsx
+++ apps/portfolio-website/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@shared/ui/globals.css';
+import '../styles.css';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,6 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
