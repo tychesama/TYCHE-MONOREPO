9ec3fc95f94824d1aaa5ef5e3935c61a9b2320b6
tychesama <jridpan1225@gmail.com>
2026-03-03 14:00:00 +0800

    fix: blog - flipster removed interaction

--- apps/blog/src/app/components/sections/HeroFlipsterCarousel.tsx
+++ apps/blog/src/app/components/sections/HeroFlipsterCarousel.tsx
@@ -65,7 +65,7 @@ export default function HeroFlipsterCarousel({
         <button
           key={s.src + i}
           type="button"
-          onClick={() => setActive(i)}
+          // onClick={() => setActive(i)}
           className="absolute left-1/2 top-1/2 rounded-lg focus:outline-none"
           style={{
             width,
