1e42873aa7ee2f304eee8dda8e15c4ed233e2d72
tychesama <jridpan1225@gmail.com>
2026-03-20 18:48:12 +0800

    fix(portfolio): resend api key move to handler

--- apps/portfolio-website/src/app/api/contact/route.ts
+++ apps/portfolio-website/src/app/api/contact/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
+
 
 export async function POST(req: Request) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY!);
+    
     const body = await req.json();
     const { name, email, message, token } = body;
 
