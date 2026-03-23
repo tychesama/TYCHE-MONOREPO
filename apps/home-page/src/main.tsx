7afe12f0646fc8d7595b3affaae2e147cfaefaa9
tychesama <jridpan1225@gmail.com>
2026-03-23 23:15:38 +0800

    fix(landing): vercel

--- apps/home-page/src/main.tsx
+++ apps/home-page/src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import '@shared/ui/globals.css'
 import LandingPage from './LandingPage'
 import { BrowserRouter } from 'react-router-dom'
-import ThemeSwitcher from '@shared/ui/ThemeSwitcher';
 import BackgroundHost from '@shared/ui/BackgroundHost';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeSwitcher />
       <BackgroundHost />
       <LandingPage />
     </BrowserRouter>
